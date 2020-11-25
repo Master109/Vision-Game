9225499db32dc1f9409420eb271160dc7b96d74c
Master109 <gileadcosman@gmail.com>
2020-11-24 21:42:26 -0800

    Continued: Make the gameplay menu #14

--- Assets/Standard Assets/Scripts/Concepts/MonoBehaviours/GameplayMenu.cs
+++ Assets/Standard Assets/Scripts/Concepts/MonoBehaviours/GameplayMenu.cs
@@ -21,6 +21,16 @@ namespace VisionGame
 		public Option[] options = new Option[0];
 		public Transform selectorTrs;
 		Option selectedOption;
+		bool leftGameplayMenuInput;
+		bool previousLeftGameplayMenuInput;
+		bool rightGameplayMenuInput;
+		bool previousRightGameplayMenuInput;
+
+		void Awake ()
+		{
+			instance = this;
+			gameObject.SetActive(false);
+		}
 
 		void OnEnable ()
 		{
@@ -29,15 +39,73 @@ namespace VisionGame
 
 		public void DoUpdate ()
 		{
-			if ((selectorTrs.position - centerOptionRange.bounds.center).sqrMagnitude <= centerOptionRange.bounds.extents.x)
-			{
-				
-			}
+			leftGameplayMenuInput = InputManager.LeftGameplayMenuInput;
+			rightGameplayMenuInput = InputManager.RightGameplayMenuInput;
+			HandleSelecting ();
+			HandleInteracting ();
+			previousLeftGameplayMenuInput = leftGameplayMenuInput;
+			previousRightGameplayMenuInput = rightGameplayMenuInput;
 		}
 
 		void OnDisable ()
 		{
 			GameManager.updatables = GameManager.updatables.Remove(this);
+		}
+
+		void HandleSelecting ()
+		{
+			if ((selectorTrs.position - centerOptionRange.bounds.center).sqrMagnitude <= centerOptionRange.bounds.extents.x)
+			{
+				Select (selectedOption);
+			}
+			else
+			{
+				
+			}
+		}
+
+		void HandleInteracting ()
+		{
+			if (!selectedOption.Equals(default(Option)))
+			{
+				if (selectorTrs == Player.Instance.leftHandTrs)
+				{
+					if (leftGameplayMenuInput && !previousLeftGameplayMenuInput)
+					{
+
+					}
+				}
+				else
+				{
+					if (rightGameplayMenuInput && !previousRightGameplayMenuInput)
+					{
+						
+					}
+				}
+			}
+		}
+
+		void Select (Option option)
+		{
+			if (!selectedOption.Equals(default(Option)))
+				Deselect (selectedOption);
+			if (option.isInteractive)
+			{
+				selectedOption = option;
+				selectedOption.deselectedGo.SetActive(false);
+				selectedOption.selectedGo.SetActive(true);
+			}
+			else
+				selectedOption = default(Option);
+		}
+
+		void Deselect (Option option)
+		{
+			if (option.isInteractive)
+			{
+				option.selectedGo.SetActive(false);
+				option.deselectedGo.SetActive(true);
+			}
 		}
 
 		// void HandleOrbViewing ()
@@ -60,10 +128,11 @@ namespace VisionGame
 		// }
 
 		[Serializable]
-		public class Option
+		public struct Option
 		{
 			public GameObject selectedGo;
 			public GameObject deselectedGo;
+			public bool isInteractive;
 			public GameObject uninteractiveGo;
 			public UnityEvent interactUnityEvent;
 		}
