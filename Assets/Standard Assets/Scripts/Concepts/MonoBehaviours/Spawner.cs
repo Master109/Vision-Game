616eb2f8a18cdf76ba37c5ac5d988ec77b3796e7
Gilead Cosman <gileadcosman@gmail.com>
2020-11-26 11:54:38 -0800

    Done: Make traveling bending line segments #17

--- Assets/Standard Assets/Scripts/Concepts/MonoBehaviours/Spawner.cs
+++ Assets/Standard Assets/Scripts/Concepts/MonoBehaviours/Spawner.cs
@@ -1,36 +1,54 @@
 using UnityEngine;
 using Extensions;
 using VisionGame;
+using System;
+using Random = UnityEngine.Random;
 
 public class Spawner : MonoBehaviour
 {
-    public int prefabIndex;
-    public BoxCollider[] spawnBoxes = new BoxCollider[0];
-    public Timer spawnTimer;
-    public float prefabRadius;
+	public int prefabIndex;
+	public SpawnZone[] spawnZones = new SpawnZone[0];
+	public Timer spawnTimer;
+	public float prefabRadius;
 	public LayerMask whatICantSpawnIn;
 
 	void OnEnable ()
 	{
-        spawnTimer.onFinished += Spawn;
-        spawnTimer.Reset ();
-        spawnTimer.Start ();
+		spawnTimer.onFinished += Spawn;
+		spawnTimer.Reset ();
+		spawnTimer.Start ();
 	}
 
-    void Spawn (params object[] args)
-    {
-        Vector3 spawnPosition;
-        do
-        {
-            BoxCollider spawnBox = spawnBoxes[Random.Range(0, spawnBoxes.Length)];
-            spawnPosition = spawnBox.bounds.RandomPoint();
-        } while (Physics.CheckSphere(spawnPosition, prefabRadius, whatICantSpawnIn));
-        ObjectPool.instance.SpawnComponent<ISpawnable>(prefabIndex, spawnPosition, Quaternion.LookRotation(Random.onUnitSphere));
-    }
+	void Spawn (params object[] args)
+	{
+		Vector3 spawnPosition;
+		do
+		{
+			SpawnZone spawnZone = spawnZones[Random.Range(0, spawnZones.Length)];
+			spawnPosition = spawnZone.boxCollider.bounds.RandomPoint();
+			for (int i = 0; i < spawnZone.points.Length; i ++)
+			{
+				Transform point = spawnZone.points[i];
+				Ray ray = new Ray(spawnPosition, point.position - spawnPosition);
+				if (!Physics.SphereCast(ray, prefabRadius, (point.position - spawnPosition).magnitude, whatICantSpawnIn))
+				{
+					ObjectPool.instance.SpawnComponent<ISpawnable>(prefabIndex, spawnPosition, Quaternion.LookRotation(Random.onUnitSphere));
+					return;
+				}
+			}
+		} while (true);
+	}
 
 	void OnDisable ()
 	{
-        spawnTimer.Stop ();
-        spawnTimer.onFinished -= Spawn;
+		spawnTimer.Stop ();
+		spawnTimer.onFinished -= Spawn;
+	}
+
+	[Serializable]
+	public struct SpawnZone
+	{
+		public BoxCollider boxCollider;
+		public Transform[] points;
 	}
 }
